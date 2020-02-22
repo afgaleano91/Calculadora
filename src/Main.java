987822ec3dc0fa222cda33ea3443ce68fd41fbf8
Andres <galeanoa8@gmailcom>
2020-02-22 01:14:22 -0500

    Ajustando el proyecto

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+		int x = 23;
+		int a = 12;
+		
+		// Primeros pasos
+
+	}
+
+}
